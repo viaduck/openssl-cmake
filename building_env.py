ae4a9cd5521c0becdc2083d35dec3e933317659c
Steffen Klee <steffen.klee@gmail.com>
2015-10-18 21:12:04 +0200

    Initial commit

---
+++ building_env.py
@@ -0,0 +1,45 @@
+# creates a building environment for openssl
+# - working directory
+# - on windows: uses msys' bash for command execution (openssl's scripts need an UNIX-like environment with perl)
+
+from subprocess import PIPE, Popen
+from sys import argv, exit
+import os
+
+env = os.environ
+l = []
+
+os_s = argv[1]                                      # operating system
+offset = 2          # 0: this script's path, 1: operating system
+
+if os_s == "WIN32":
+    offset = 4  # 2: MSYS_BASH_PATH, 3: CMAKE_MAKE_PROGRAM
+
+    #
+    bash = argv[2]
+    msys_path = os.path.dirname(bash)
+    mingw_path = os.path.dirname(argv[3])
+
+    # append ; to PATH if needed
+    if not env['PATH'].endswith(";"):
+        env['PATH'] += ";"
+
+    # include path of msys binaries (perl, cd etc.) and building tools (gcc, ld etc.)
+    env['PATH'] += ";".join([msys_path, mingw_path])
+    env['MAKEFLAGS'] = ''            # otherwise: internal error: invalid --jobserver-fds string `gmake_semaphore_1824'
+
+binary_openssl_dir_source = argv[offset]+"/"             # downloaded openssl source dir
+l.extend(argv[offset+1:])                             # routed commands
+
+l[0] = '"'+l[0]+'"'
+
+proc = None
+if os_s == "WIN32":
+    # we must emulate a UNIX environment to build openssl using mingw
+    proc = Popen(bash, env=env, cwd=binary_openssl_dir_source, stdin=PIPE, universal_newlines=True)
+    proc.communicate(input=" ".join(l)+" || exit $?")
+else:
+    proc = Popen(" ".join(l)+" || exit $?", shell=True, env=env, cwd=binary_openssl_dir_source)
+    proc.communicate()
+
+exit(proc.returncode)
---
+++ openssl-config.cmake
@@ -0,0 +1,3 @@
+macro(config_openssl)
+    include_directories(${CMAKE_CURRENT_BINARY_DIR}/openssl/include)
+endmacro()
