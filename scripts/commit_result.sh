67aefaaf49bd2d53cfad4dc4bfe2d2fbe27fcf1d
Steffen Klee <steffen.klee@gmail.com>
2015-11-29 04:18:51 +0100

    Updated CI build scripts with cross-compilation support

--- scripts/commit_result.sh
+++ scripts/commit_result.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
-git clone git@g82.primeghost.de:secchatgroup/openssl-prebuilts.git || exit 1
+# usage: <architecture> <directory>
 
 cd openssl-prebuilts
+git checkout master
 git branch $1
 git checkout $1
 
 rm -R $1/* || mkdir $1
-cp -R ../bin $1
-cp -R ../lib $1
-cp -R ../include $1
-cp -R ../ssl $1
+cp -R ../$2/bin $1
+cp -R ../$2/lib $1
+cp -R ../$2/include $1
+cp -R ../$2/ssl $1
 
 git add $1
-git commit -m "Automatically built by slave ($2)" > /dev/null
+git commit -m "Automatically built by slave ($BUILD_OPENSSL_VERSION)" > /dev/null
 git push --set-upstream origin $1
