da1e810c45f2af1f6ef390f4bfe55cb16dd973c9
Steffen Klee <steffen.klee@gmail.com>
2016-03-15 04:09:03 +0100

    Fine-tuned OpenSSL build detection
    CMake now tries to find the already built OpenSSL libraries in binary dir if PREBUILT_BRANCH is not specified and sets the target properties accordingly.

--- openssl-config.cmake
+++ openssl-config.cmake
@@ -1,6 +1,6 @@
 macro(config_openssl opensslPath)
-    if(BUILD_OPENSSL)
-        include_directories(${CMAKE_CURRENT_BINARY_DIR}/openssl/include)
+    if(NOT PREBUILT_BRANCH)
+        include_directories(${CMAKE_CURRENT_BINARY_DIR}/${opensslPath}/include)
     else()
         include_directories(${CMAKE_CURRENT_BINARY_DIR}/${opensslPath}/openssl-prefix/src/openssl/${PREBUILT_BRANCH}/include)
     endif()
