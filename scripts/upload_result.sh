b1f0c339eb26d54751d8c63ab6b84cc1a7c57687
Steffen Klee <steffen.klee@gmail.com>
2019-08-06 02:19:54 +0200

    [ci skip] Fixed HTTP code check when uploading

--- scripts/upload_result.sh
+++ scripts/upload_result.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # MIT License
 #
-# Copyright (c) 2018 The ViaDuck Project
+# Copyright (c) 2018-2019 The ViaDuck Project
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -38,7 +38,7 @@ tar czf $1.tar.gz $1
 { code=$(curl -u $PREBUILT_AUTH -F "file=@$1.tar.gz" -F "dir=prebuilts/openssl/$OPENSSL_BUILD_VERSION" -F 'checksum=yes' -o /dev/stderr -w '%{http_code}' https://mirror.viaduck.org/scripts/upload.py); } 2>&1
 
 # check for 200
-if test $code -ne 200; then
+if [ "$code" -ne 200 ]; then
     echo "cURL error"
     exit 1
 fi
