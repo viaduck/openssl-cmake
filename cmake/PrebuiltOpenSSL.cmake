79c122d1606556610477cfae07ff27d8c6e5f260
Alexandros Roussos <thealexroussos@gmail.com>
2023-03-02 02:00:36 +0100

    Updated to OpenSSL 1.1.1t
    * Added patch fixing a failing test, see https://github.com/openssl/openssl/issues/20249

--- cmake/PrebuiltOpenSSL.cmake
+++ cmake/PrebuiltOpenSSL.cmake
@@ -50,7 +50,7 @@ set(PREBUILT_BRANCH ${ARCH_SYSTEM} CACHE STRING "Branch in OpenSSL-Prebuilts to 
 
 # auto version
 if (NOT OPENSSL_PREBUILT_VERSION)
-    set(OPENSSL_PREBUILT_VERSION "1.1.1s")
+    set(OPENSSL_PREBUILT_VERSION "1.1.1t")
 endif()
 
 # add openssl target
